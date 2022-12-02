d9432cdcc4afa1179959e28fd448ede7c8a9466c
s-shah-coder <saf.h.shah@gmail.com>
2022-12-02 10:19:05 +0000

    added variable for dayscomplete and text

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import "fmt"
 func main() {
     var challenge = "#90DaysOfDevOps"
     const daystotal = 90
+    var dayscomplete = 11
 
     fmt.Println("Welcome to", challenge, "")
-    fmt.Println("This is a", daystotal, "challenge")
+    fmt.Println("This is a", daystotal, "challenge and you have completed", dayscomplete, "days")
+    fmt.Println("Great work")
 }
